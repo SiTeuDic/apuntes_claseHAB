c985a257e225f567aae3e78a7e6a1bd3df4511b7
Nacho <siteudic@gmail.com>
2022-11-02 18:28:32 +0100

    a

--- Semana_5/E_s5/pizza/index.js
+++ Semana_5/E_s5/pizza/index.js
@@ -13,14 +13,15 @@ function combine(pizzas) {
   const combinations = [];
   for (let i = 0; i < pizzas.length; i++) {
     const element = pizzas[i];
-
     for (let i = 1; i < pizzas.length; i++) {
       const element2 = pizzas[i];
       if (element !== element2) {
         combinations.push(element + " y " + element2);
       }
+    
     }
   }
+
   return combinations;
 }
 
